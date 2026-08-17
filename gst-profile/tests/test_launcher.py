a50febe55ad97c96fcd1880b8a2c62be90ff1bcf
ProventusNova <admin@proventusnova.com>
2026-08-17 17:35:37 -0600

    Launch pipelines under the tracer environment with FIFO capture and clean shutdown

---
+++ gst-profile/tests/test_launcher.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import sys
+import tempfile
+import textwrap
+import time
+import unittest
+from gst_profile.launcher import Launcher, build_env, TRACERS_FULL, TRACERS_OLD
+
+FAKE_CHILD = textwrap.dedent('''
+    import os, sys, time
+    dot_dir = os.environ["GST_DEBUG_DUMP_DOT_DIR"]
+    with open(os.path.join(dot_dir, "0.00.00.1-fake.PAUSED_PLAYING.dot"), "w") as fh:
+        fh.write("digraph pipeline {\\n  label=\\"<GstPipeline>\\\\npipeline0\\\\n[>]\\";\\n}\\n")
+    with open(os.environ["GST_DEBUG_FILE"], "w") as fh:
+        for i in range(50):
+            fh.write("0:00:00.%09d 1 0x1 TRACE GST_TRACER :0:: element-latency, element-id=(string)0x1, element=(string)e, src=(string)src, time=(guint64)1000, ts=(guint64)%d;\\n" % (i * 1000, i * 1000))
+            fh.flush()
+            time.sleep(0.01)
+    sys.exit(7)
+''')
+
+
+class LauncherTest(unittest.TestCase):
+    def test_build_env(self):
+        env = build_env({"PATH": "/bin"}, "/tmp/f", "/tmp/d", element_latency=True, mode="wrap")
+        self.assertEqual(env["GST_TRACERS"], TRACERS_FULL)
+        self.assertEqual(env["GST_DEBUG"], "GST_TRACER:7,GST_EVENT:5")
+        self.assertEqual(env["GST_DEBUG_FILE"], "/tmp/f")
+        self.assertEqual(env["GST_DEBUG_DUMP_DOT_DIR"], "/tmp/d")
+        self.assertEqual(env["GST_DEBUG_NO_COLOR"], "1")
+        env = build_env({}, "/tmp/f", "/tmp/d", element_latency=False, mode="run")
+        self.assertEqual(env["GST_TRACERS"], TRACERS_OLD)
+        self.assertEqual(env["GST_DEBUG"], "GST_TRACER:7")
+
+    def test_fifo_and_dot_pumped_and_exit_code(self):
+        tmp = tempfile.mkdtemp()
+        script = os.path.join(tmp, "child.py")
+        with open(script, "w") as fh:
+            fh.write(FAKE_CHILD)
+        lines, dots = [], []
+        l = Launcher([sys.executable, script], lines.append, dots.append, mode="wrap", workdir=tmp)
+        l.start()
+        self.assertIsNotNone(l.wait(timeout=10))
+        code = l.stop()
+        l.cleanup()
+        self.assertEqual(code, 7)
+        self.assertEqual(len(lines), 50)
+        self.assertEqual(len(dots), 1)
+        self.assertIn("pipeline0", dots[0])
+        self.assertFalse(os.path.exists(l.fifo))
+        shutil.rmtree(tmp)
+
+    def test_stop_kills_a_stubborn_child(self):
+        tmp = tempfile.mkdtemp()
+        script = os.path.join(tmp, "stubborn.py")
+        with open(script, "w") as fh:
+            fh.write("import signal, time\nsignal.signal(signal.SIGINT, signal.SIG_IGN)\nsignal.signal(signal.SIGTERM, signal.SIG_IGN)\ntime.sleep(60)\n")
+        l = Launcher([sys.executable, script], lambda s: None, lambda s: None, workdir=tmp)
+        l.start()
+        time.sleep(0.3)
+        t0 = time.time()
+        code = l.stop(grace=0.5)
+        self.assertLess(time.time() - t0, 10)
+        self.assertNotEqual(code, 0)
+        self.assertFalse(l.running())
+        shutil.rmtree(tmp)
+
+
+if __name__ == "__main__":
+    unittest.main()
